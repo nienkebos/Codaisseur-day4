cc2b093f3957c39bfaf152291a2263d9415df667
nienkebos <nienkebos@live.com>
2016-08-03 11:05:04 +0200

    stap2

--- pizzeria.rb
+++ pizzeria.rb
@@ -1,5 +1,5 @@
 require './waiter'
 w = Waiter.new
 w.greet_guest
-w.serve_guest
-p w.take_order
+order_number = w.serve_guest
+p w.take_order(order_number)
--- waiter.rb
+++ waiter.rb
@@ -1,20 +1,26 @@
 class Waiter
+
   def greet_guest
     p "Good day. Welcome to our lovely restaurant"
   end
+
   def serve_guest
     p "How can I be of service?"
     p "1. Would you like to order a pizza?"
     p "2. Would you like to leave?"
     choice = gets.chomp.to_i
   end
-#   def take_order(order_number)
-#     case order_number
-#     when 1
-#       "Let me get the menu..."
-#     when 2
-#       "Thank you for your visit."
-#     else
-#       "Sorry, I don't understand you."
-#   end
-# end
+
+  def take_order(order_number)
+    case order_number
+    when 1
+      "Let me get the menu..."
+    when 2
+      "Thank you for your visit."
+    else
+      "Sorry, I don't understand you."
+    end
+    take_order(gets.chomp.to_i)
+  end
+
+end
